b46f6bc4df66907034d1cb91fd16aa59c1c5fbec
Robert de Kok <pa2rdk@rjdekok.nl>
2018-08-01 22:58:27 +0200

    Valid GPS

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-07-14 11:39:12
+//This file has been generated on 2018-08-01 17:26:59
 
 #include "Arduino.h"
 #include <RDKAPRS.h>
